35730856461a9ba7ec559c8d805aee36f9520bcf
swr2photo <146337142+swr2photo@users.noreply.github.com>
2025-05-08 00:25:42 +0000

    up

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-  }
-  
-  module.exports = nextConfig;
-  
+const withCloudflarePages = require('@cloudflare/next-on-pages');
+
+module.exports = withCloudflarePages({
+  target: 'serverless', // เป็นการตั้งค่าที่จำเป็นสำหรับการ deploy บน Cloudflare Workers
+  // การตั้งค่าอื่น ๆ ของ Next.js ที่คุณอาจต้องการ
+});
